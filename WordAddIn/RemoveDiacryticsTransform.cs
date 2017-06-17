b785e35f8e00cd4cbd68f5d628b89cb4317dd74f
Ananta Krsna das <anantakrishna.kem@gmail.com>
2017-06-17 09:44:19 +0700

    Remove Diacritics draft.

---
+++ WordAddIn/RemoveDiacryticsTransform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.Office.Interop.Word;
+
+namespace GaudiaVedantaPublications
+{
+    public class RemoveDiacryticsTransform : ITextTransform
+    {
+        public void Apply(Range range)
+        {
+            range.Text = Regex.Replace(range.Text, @"\p{IsCombiningDiacriticalMarks}", String.Empty);
+        }
+    }
+}
